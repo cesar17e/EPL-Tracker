c1c342410732c5133e7397067bfe75080b08504c
Cesar Estrada <cesar176e@gmail.com>
2026-03-21 02:56:13 -0400

    Fixed Image mistake

--- src/scripts/backfillTeamLogos.ts
+++ src/scripts/backfillTeamLogos.ts
@@ -3,6 +3,7 @@ import { pool } from "../db/pool.js";
 
 const TEAM_LOGOS: Record<string, string> = {
   arsenal: "https://upload.wikimedia.org/wikipedia/en/5/53/Arsenal_FC.svg",
+  "aston-villa": "https://upload.wikimedia.org/wikipedia/en/9/9a/Aston_Villa_FC_new_crest.svg",
   bournemouth: "https://upload.wikimedia.org/wikipedia/sco/e/e5/AFC_Bournemouth_%282013%29.svg",
   brentford: "https://upload.wikimedia.org/wikipedia/en/2/2a/Brentford_FC_crest.svg",
   brighton: "https://upload.wikimedia.org/wikipedia/sco/f/fd/Brighton_%26_Hove_Albion_logo.svg",
@@ -14,6 +15,7 @@ const TEAM_LOGOS: Record<string, string> = {
   "leeds-united": "https://upload.wikimedia.org/wikipedia/en/5/54/Leeds_United_F.C._logo.svg",
   liverpool: "https://upload.wikimedia.org/wikipedia/sco/0/0c/Liverpool_FC.svg",
   "manchester-city": "https://upload.wikimedia.org/wikipedia/sco/e/eb/Manchester_City_FC_badge.svg",
+  "manchester-united": "https://upload.wikimedia.org/wikipedia/en/7/7a/Manchester_United_FC_crest.svg",
   "newcastle-united": "https://upload.wikimedia.org/wikipedia/sco/5/56/Newcastle_United_Logo.svg",
   "nottingham-forest": "https://upload.wikimedia.org/wikipedia/sco/d/d2/Nottingham_Forest_logo.svg",
   sunderland: "https://upload.wikimedia.org/wikipedia/sco/7/77/Logo_Sunderland.svg",
